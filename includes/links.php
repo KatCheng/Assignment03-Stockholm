cf43d243d1514b4a62cfa29e7ae9e16b5caa03b3
apple <apple@dhcp-10-5-28-184.wireless.rochester.edu>
2018-02-22 16:09:46 -0500

    design

--- includes/links.php
+++ includes/links.php
@@ -1,1 +1,8 @@
- 
+ <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./bootstrap/css/custom.css">
+<link rel="stylesheet" type="text/css" href="./bootstrap/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="./bootstrap/css/responsiveslides.css">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+  
