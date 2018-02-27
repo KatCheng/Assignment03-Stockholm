7860267ff0d8b0da1c16c8a2c0b72087b7204c85
Valerie <xwang145@u.rochester.edu>
2018-02-27 16:47:25 -0500

    updated scripts php include

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,5 +1,6 @@
-  <script type="text/javascript" src="js/menu-highlighter.js"></script>
+  <!-- <script src="http://code.jquery.com/jquery.js"></script> -->
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+  <script type="text/javascript" src="js/menu-highlighter.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
   <script src="js/responsiveslides.js"></script>    
     <script>
