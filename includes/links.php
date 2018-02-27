97e520f34e9d7a6c7312a0fb8956efb90b2c39ad
Valerie <xwang145@u.rochester.edu>
2018-02-27 17:03:38 -0500

    fixed "is current" display

--- includes/links.php
+++ includes/links.php
@@ -1,8 +1,8 @@
 
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="./css/responsiveslides.css">
 <link rel="stylesheet" type="text/css" href="/css/styles.css">
+<link rel="stylesheet" type="text/css" href="/css/navigation.css">
 <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
   
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,7 +1,6 @@
-  <!-- <script src="http://code.jquery.com/jquery.js"></script> -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+  <script src="http://code.jquery.com/jquery.js"></script>
   <script type="text/javascript" src="js/menu-highlighter.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+  <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script> -->
   <script src="js/responsiveslides.js"></script>    
     <script>
           $(function() {
