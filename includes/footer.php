a2eb91a0f8ee8bb13593660a3c0868e6623160d1
Yeonwoo Lee <ylee100@u.rochester.edu>
2018-04-01 18:59:12 -0400

    Deleted Unnecessary Files

--- includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer">
-  <span class="text-muted">Page created by Team Stockholm</span>
-  
-</footer>
--- includes/scripts.php
+++
@@ -1,9 +0,0 @@
-  <script src="http://code.jquery.com/jquery.js"></script>
-  <script type="text/javascript" src="js/menu-highlighter.js"></script>
-  <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script> -->
-  <script src="js/responsiveslides.js"></script>    
-    <script>
-          $(function() {
-          $(".rslides").responsiveSlides();
-          });
-    </script>
